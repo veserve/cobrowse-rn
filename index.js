ff81d3a5e94b59e8c8d4957b68b3725804d8e9be
VeServe Joeson <joesonau@veservecompany.com>
2022-05-30 11:32:52 +0800

    add userId inside

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ class Cobrowse {
   constructor() {
     this.license = null;
     this.api = null;
+    this.started = false;
   }
 
-  start(opts = {}) {
+  setUserId(userId) {
+    CBEngine.customData = {
+      user_id: userId,
+      cobrowseId: userId,
+    };
+  }
+
+  async getSixDigitCode() {
+    if (!this.started) {
+      return null;
+    }
+    const session = await CBEngine.createSession();
+    return session.code;
+  }
+
+  start() {
     CBEngine.license = this.license;
     CBEngine.api = this.api;
     CBEngine.start();
+    this.started = true;
   }
 }
 
